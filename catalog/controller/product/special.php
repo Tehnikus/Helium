4f5ac5cc1b27970331f787643c76aa1d3b50095a
Tehnikus <tehnikus.ua@gmail.com>
2023-05-09 16:20:12 +0200

    PHP: Special product page prepareProductList()

--- catalog/controller/product/special.php
+++ catalog/controller/product/special.php
@@ -97,55 +97,16 @@ class ControllerProductSpecial extends Controller {
 
 		$product_total = $this->model_catalog_product->getTotalProductSpecials();
 
-		$results = $this->model_catalog_product->getProductSpecials($filter_data);
 
-		foreach ($results as $result) {
-			if ($result['image']) {
-				$image = $this->model_tool_image->resize($result['image'], $this->config->get('image_product_width'), $this->config->get('image_product_height'));
-			} else {
-				$image = $this->model_tool_image->resize('no_image.webp', $this->config->get('image_product_width'), $this->config->get('image_product_height'));
+		$special_products = $this->model_catalog_product->getProductSpecials($filter_data);
+		if ($special_products) {
+			foreach ($special_products as $product_id) {
+				$product_list[] = $this->model_catalog_product->getProduct($product_id['product_id']);
 			}
-
-			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-				$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-			} else {
-				$price = false;
-			}
-
-			if (!is_null($result['special']) && (float)$result['special'] >= 0) {
-				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-				$tax_price = (float)$result['special'];
-			} else {
-				$special = false;
-				$tax_price = (float)$result['price'];
-			}
-
-			if ($this->config->get('config_tax')) {
-				$tax = $this->currency->format($tax_price, $this->session->data['currency']);
-			} else {
-				$tax = false;
-			}
-
-			if ($this->config->get('config_review_status')) {
-				$rating = (int)$result['rating'];
-			} else {
-				$rating = false;
-			}
-
-			$data['products'][] = array(
-				'product_id'  => $result['product_id'],
-				'thumb'       => $image,
-				'name'        => $result['name'],
-				'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
-				'price'       => $price,
-				'special'     => $special,
-				'tax'         => $tax,
-				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-				'rating'      => $result['rating'],
-				'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url)
-			);
+			$data['products'] = $this->model_catalog_product->prepareProductList($product_list, null);
 		}
 
+		
 		$url = '';
 
 		if (isset($this->request->get['limit'])) {
