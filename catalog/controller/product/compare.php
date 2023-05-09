dd6d4e7ceb444f237b5020366d4979d6018cd6b5
Tehnikus <tehnikus.ua@gmail.com>
2023-05-09 15:42:11 +0200

    Minor fixes

--- catalog/controller/product/compare.php
+++ catalog/controller/product/compare.php
@@ -165,7 +165,7 @@ class ControllerProductCompare extends Controller {
 						'thumb'        => $image,
 						'price'        => $price,
 						'special'      => $special,
-						'description'  => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..',
+						'description'  => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '...',
 						'model'        => $product_info['model'],
 						'manufacturer' => $product_info['manufacturer'],
 						'availability' => $availability,
