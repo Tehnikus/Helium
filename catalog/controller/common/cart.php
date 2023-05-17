b7925ba46a3afa4144460d87d480021b2428fe77
Tehnikus <tehnikus.ua@gmail.com>
2023-05-18 00:14:11 +0200

    PHP: WIP: Dispaly quick checkout form
    Work in progress!
    Adding PHP function to display quick checkout. Fill fields with session data, if user saved something and abandoned cart. So if checkout process is continiued, all fields will be filled accordingly

--- catalog/controller/common/cart.php
+++ catalog/controller/common/cart.php
@@ -315,4 +315,54 @@ class ControllerCommonCart extends Controller {
 	public function fetchProductCount() {
 		echo($this->cart->countProducts());
 	}
+
+	public function displayQuickCheckout() {
+		$this->load->language('checkout/checkout');
+		$this->load->model('account/address');
+		$this->load->model('localisation/country');
+		$this->load->model('account/custom_field');
+
+		$data['addresses'] = $this->model_account_address->getAddresses();
+
+		if (isset($this->session->data['payment_address']['address_id'])) {
+			$data['address_id'] = $this->session->data['payment_address']['address_id'];
+		} else {
+			$data['address_id'] = $this->customer->getAddressId();
+		}
+
+
+		if (isset($this->session->data['payment_address']['country_id'])) {
+			$data['country_id'] = $this->session->data['payment_address']['country_id'];
+		} else {
+			$data['country_id'] = $this->config->get('config_country_id');
+		}
+
+		if (isset($this->session->data['payment_address']['zone_id'])) {
+			$data['zone_id'] = $this->session->data['payment_address']['zone_id'];
+		} else {
+			$data['zone_id'] = '';
+		}
+
+		
+
+		$data['countries'] = $this->model_localisation_country->getCountries();
+
+		// Custom Fields
+		$data['custom_fields'] = array();
+		$custom_fields = $this->model_account_custom_field->getCustomFields($this->config->get('config_customer_group_id'));
+
+		foreach ($custom_fields as $custom_field) {
+			if ($custom_field['location'] == 'address') {
+				$data['custom_fields'][] = $custom_field;
+			}
+		}
+
+		if (isset($this->session->data['payment_address']['custom_field'])) {
+			$data['payment_address_custom_field'] = $this->session->data['payment_address']['custom_field'];
+		} else {
+			$data['payment_address_custom_field'] = array();
+		}
+
+		echo($this->load->view('common/quick_checkout', $data));
+	}
 }
