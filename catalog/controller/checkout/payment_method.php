24b3c40df361f737bcd73ba9da302dc0587037e3
Tehnikus <tehnikus.ua@gmail.com>
2023-05-25 18:41:00 +0200

    PHP: Payment methods comply quick checkout

--- catalog/controller/checkout/payment_method.php
+++ catalog/controller/checkout/payment_method.php
@@ -3,8 +3,73 @@
 
 class ControllerCheckoutPaymentMethod extends Controller {
 	public function index() {
+		
+		$data = $this->getPaymentMethodsData();
+		$this->response->setOutput($this->load->view('checkout/payment_method', $data));
+	}
+
+	public function save() {
 		$this->load->language('checkout/checkout');
 
+		$json = array();
+
+		// Validate if payment address has been set.
+		if (!isset($this->session->data['payment_address'])) {
+			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
+		}
+
+		// Validate cart has products and has stock.
+		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+			$json['redirect'] = $this->url->link('checkout/cart');
+		}
+
+		// Validate minimum quantity requirements.
+		$products = $this->cart->getProducts();
+
+		foreach ($products as $product) {
+			$product_total = 0;
+
+			foreach ($products as $product_2) {
+				if ($product_2['product_id'] == $product['product_id']) {
+					$product_total += $product_2['quantity'];
+				}
+			}
+
+			if ($product['minimum'] > $product_total) {
+				$json['redirect'] = $this->url->link('checkout/cart');
+
+				break;
+			}
+		}
+
+		if (!isset($this->request->post['payment_method'])) {
+			$json['error']['warning'] = $this->language->get('error_payment');
+		} elseif (!isset($this->session->data['payment_methods'][$this->request->post['payment_method']])) {
+			$json['error']['warning'] = $this->language->get('error_payment');
+		}
+
+		if ($this->config->get('config_checkout_id')) {
+			$this->load->model('catalog/information');
+
+			$information_info = $this->model_catalog_information->getInformation($this->config->get('config_checkout_id'));
+
+			if ($information_info && !isset($this->request->post['agree'])) {
+				$json['error']['warning'] = sprintf($this->language->get('error_agree'), $information_info['title']);
+			}
+		}
+
+		if (!$json) {
+			$this->session->data['payment_method'] = $this->session->data['payment_methods'][$this->request->post['payment_method']];
+
+			$this->session->data['comment'] = strip_tags($this->request->post['comment']);
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+
+	public function getPaymentMethodsData() {
+		$this->load->language('checkout/checkout');
 		if (isset($this->session->data['payment_address'])) {
 			// Totals
 			$totals = array();
@@ -122,67 +187,10 @@ class ControllerCheckoutPaymentMethod extends Controller {
 		} else {
 			$data['agree'] = '';
 		}
-
-		$this->response->setOutput($this->load->view('checkout/payment_method', $data));
+		return $data;
 	}
-
-	public function save() {
-		$this->load->language('checkout/checkout');
-
-		$json = array();
-
-		// Validate if payment address has been set.
-		if (!isset($this->session->data['payment_address'])) {
-			$json['redirect'] = $this->url->link('checkout/checkout', '', true);
-		}
-
-		// Validate cart has products and has stock.
-		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
-			$json['redirect'] = $this->url->link('checkout/cart');
-		}
-
-		// Validate minimum quantity requirements.
-		$products = $this->cart->getProducts();
-
-		foreach ($products as $product) {
-			$product_total = 0;
-
-			foreach ($products as $product_2) {
-				if ($product_2['product_id'] == $product['product_id']) {
-					$product_total += $product_2['quantity'];
-				}
-			}
-
-			if ($product['minimum'] > $product_total) {
-				$json['redirect'] = $this->url->link('checkout/cart');
-
-				break;
-			}
-		}
-
-		if (!isset($this->request->post['payment_method'])) {
-			$json['error']['warning'] = $this->language->get('error_payment');
-		} elseif (!isset($this->session->data['payment_methods'][$this->request->post['payment_method']])) {
-			$json['error']['warning'] = $this->language->get('error_payment');
-		}
-
-		if ($this->config->get('config_checkout_id')) {
-			$this->load->model('catalog/information');
-
-			$information_info = $this->model_catalog_information->getInformation($this->config->get('config_checkout_id'));
-
-			if ($information_info && !isset($this->request->post['agree'])) {
-				$json['error']['warning'] = sprintf($this->language->get('error_agree'), $information_info['title']);
-			}
-		}
-
-		if (!$json) {
-			$this->session->data['payment_method'] = $this->session->data['payment_methods'][$this->request->post['payment_method']];
-
-			$this->session->data['comment'] = strip_tags($this->request->post['comment']);
-		}
-
-		$this->response->addHeader('Content-Type: application/json');
-		$this->response->setOutput(json_encode($json));
+	public function fetchPaymentMethodsData() {
+		$data = $this->getPaymentMethodsData();
+		print_r($data);
 	}
 }
