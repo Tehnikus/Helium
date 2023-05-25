73c2e33376fe7e73212d4b674f17101fe7c9cdc3
Tehnikus <tehnikus.ua@gmail.com>
2023-05-25 19:01:03 +0200

    PHP: Add corresponding quick checkout errors text

--- catalog/language/en-gb/common/cart.php
+++ catalog/language/en-gb/common/cart.php
@@ -6,3 +6,14 @@ $_['text_checkout']         = 'Checkout';
 $_['text_recurring']        = 'Payment Profile';
 $_['text_header_cart']      = 'Cart';
 $_['entry_qty']             = 'Qty';
+
+$_['error_firstname']       = 'First Name must be between 1 and 32 characters';
+$_['error_lastname']        = 'Last Name must be between 1 and 32 characters';
+$_['error_address_1']       = 'Address must be between 3 and 128 characters';
+$_['error_postcode']        = 'Postcode must be between 2 and 10 characters';
+$_['error_country']         = 'Please select a country';
+$_['error_zone']            = 'Please select a region / state';
+$_['error_city']            = 'City must be between 2 and 128 characters';
+$_['error_custom_field']    = '%s required';
+$_['error_shipping']        = 'Выберите способ доставки';
+$_['error_no_shipping']     = 'Доставка по данному адресу невозможна. <a href="%s">Свяжитесь с нами</a> и мы найдём решение проблемы';
