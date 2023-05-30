036cfd39bd9e8494f3172ffd8e381514bb4dc1fc
Tehnikus <tehnikus.ua@gmail.com>
2023-05-31 01:37:16 +0200

    Correct translations according to quick checkout

--- catalog/language/ru-ru/common/cart.php
+++ catalog/language/ru-ru/common/cart.php
@@ -1,19 +1,47 @@
 <?php
-$_['text_items']            = '<span class="product-count">%s</span>  <span class="cart-total-summ hidden-md">%s</span>';
-$_['text_empty']            = 'Ваша корзина пуста!';
-$_['text_cart']             = 'Перейти в корзину';
-$_['text_checkout']         = 'Оформить заказ';
-$_['text_recurring']        = 'Платежный профиль';
-$_['text_header_cart']      = 'Корзина';
-$_['entry_qty']             = 'Кол-во';
+$_['text_items']                    = '<span class="product-count">%s</span>  <span class="cart-total-summ hidden-md">%s</span>';
+$_['text_empty']                    = 'Ваша корзина пуста!';
+$_['text_cart']                     = 'Перейти в корзину';
+$_['text_checkout']                 = 'Оформить заказ';
+$_['text_recurring']                = 'Платежный профиль';
+$_['text_header_cart']              = 'Корзина';
+$_['entry_qty']                     = 'Кол-во';
 
-$_['error_firstname']       = 'Имя должно быть от 1 до 32 символов';
-$_['error_lastname']        = 'Фамилия должна быть от 1 до 32 символов';
-$_['error_address_1']       = 'Адрес должен быть от 3 до 128 символов';
-$_['error_postcode']        = 'Индекс должен содержать от 2 до 10 символов';
-$_['error_country']         = 'Пожалуйста, выберите страну';
-$_['error_zone']            = 'Пожалуйста, выберите область';
-$_['error_city']            = 'Название города должно быть от 2 до 128 символов';
-$_['error_custom_field']    = 'Заполните пожалуйста %s';
-$_['error_shipping']        = 'Выберите способ доставки';
-$_['error_no_shipping']     = 'Доставка по данному адресу невозможна. <a href="%s">Свяжитесь с нами</a> и мы найдём решение проблемы';
+
+$_['text_shipping_methods']         = 'Доставка';
+$_['text_payment_methods']          = 'Оплата';
+
+// Entry
+$_['entry_email']                   = 'E-Mail';
+$_['entry_firstname']               = 'Имя';
+$_['entry_lastname']                = 'Фамилия';
+$_['entry_telephone']               = 'Телефон';
+$_['entry_address']                 = 'Выберите адрес';
+$_['entry_company']                 = 'Компания';
+$_['entry_customer_group']          = 'Группа покупателей';
+$_['entry_address_1']               = 'Адрес';
+$_['hint_address_1']                = 'Улица, номер дома';
+$_['entry_address_2']               = 'Кваритра, этаж';
+$_['entry_postcode']                = 'Почтовый индекс';
+$_['entry_city']                    = 'Город';
+$_['entry_country']                 = 'Страна';
+$_['entry_zone']                    = 'Регион / Область';
+$_['entry_newsletter']              = 'Я хочу подписаться на рассылку новостей %s.';
+$_['entry_shipping'] 	            = 'Мой адрес доставки совпадает с платёжным.';
+
+
+$_['error_firstname']               = 'Имя должно быть от 1 до 32 символов';
+$_['error_lastname']                = 'Фамилия должна быть от 1 до 32 символов';
+$_['error_address_1']               = 'Адрес должен быть от 3 до 128 символов';
+$_['error_telephone']               = 'Номер телефона должен содержать от 3 до 32 символов';
+$_['error_postcode']                = 'Индекс должен содержать от 2 до 10 символов';
+$_['error_country']                 = 'Пожалуйста, выберите страну';
+$_['error_zone']                    = 'Пожалуйста, выберите область';
+$_['error_city']                    = 'Название города должно быть от 2 до 128 символов';
+$_['error_custom_field']            = 'Заполните пожалуйста %s';
+$_['error_shipping']                = 'Выберите способ доставки';
+$_['error_no_shipping']             = 'Доставка по данному адресу невозможна. <a href="%s">Свяжитесь с нами</a> и мы найдём решение проблемы';
+$_['error_payment']                 = 'Выберите способ оплаты';
+
+$_['text_shipping_methods']         = 'Доставка';
+$_['text_payment_methods']          = 'Оплата';
