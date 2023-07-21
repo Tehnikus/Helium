42a961e00c3e22fcd49d8a23e64d58cbb9e18059
Tehnikus <tehnikus.ua@gmail.com>
2023-07-22 00:27:59 +0200

    Update English language

--- catalog/language/en-gb/information/contact.php
+++ catalog/language/en-gb/information/contact.php
@@ -10,17 +10,18 @@ $_['text_telephone'] = 'Telephone';
 $_['text_fax']       = 'Fax';
 $_['text_open']      = 'Opening Times';
 $_['text_comment']   = 'Comments';
-$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>'; 
-
+$_['text_success']   = 'Your enquiry has been successfully sent!'; 
+$_['button_submit']  = 'Send';
 // Entry
 $_['entry_name']     = 'Your Name';
 $_['entry_email']    = 'E-Mail Address';
 $_['entry_enquiry']  = 'Enquiry';
-
+$_['entry_captcha']  = 'Enter captcha';
 // Email
 $_['email_subject']  = 'Enquiry %s';
 
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'Name must be between 3 and 32 characters';
+$_['error_email']    = 'E-Mail Address does not appear to be valid';
+$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters';
+$_['error_captcha']  = 'Captcha error, mr. Robot :(';
