fdd67dd6cc95e51de40bf52f0f7773c719b6f5f0
Tehnikus <tehnikus.ua@gmail.com>
2023-11-07 14:08:51 +0100

    SQL: Count zones in countries

--- catalog/model/localisation/country.php
+++ catalog/model/localisation/country.php
@@ -1,7 +1,17 @@
 <?php
 class ModelLocalisationCountry extends Model {
 	public function getCountry($country_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$country_id . "' AND status = '1'");
+		$query = $this->db->query("
+			SELECT 
+				c.*,
+				(SELECT 
+					COUNT(z.zone_id) 
+				FROM " . DB_PREFIX . "zone z 
+				WHERE z.country_id = " . (int)$country_id . " 
+				AND z.status = '1') as zones
+			FROM " . DB_PREFIX . "country c 
+			WHERE c.status = 1 AND c.country_id = " . (int)$country_id . ";
+		");
 
 		return $query->row;
 	}
