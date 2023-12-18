f765534c398c3f0d831d47ad711fe74e83ff0ffd
Tehnikus <tehnikus.ua@gmail.com>
2023-12-18 14:51:08 +0100

    Move PL lang files to corresponding folder

--- admin/language/pl-pl/feed/openbaypro.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title']	  = 'OpenBay Pro';
-
-// Text
-$_['text_module']    = 'Moduły';
-$_['text_installed'] = 'Moduł OpenBay Pro jest już zainstalowany. Jest on dostępny w menu Rozszerzenia-> OpenBay Pro';
